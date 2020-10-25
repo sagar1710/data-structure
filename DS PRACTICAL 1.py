b9efd307839dce6e19bd3aca58fe20bfa38db710
sagar1710 <60962765+sagar1710@users.noreply.github.com>
2020-10-25 22:57:51 +0530

    Update DS PRACTICAL 1.py

--- DS PRACTICAL 1.py
+++ DS PRACTICAL 1.py
@@ -1,42 +1,34 @@
-class Search:
-    
-    def __init__(self,array,number):
-        self.lst = sorted(array)
-        self.number = number
-    def binary_search(self,lst,n,start = 0,end = -1):
-        mid = lst[end] // 2
-        if start - end == 0:
-            return 'Element not in the list'
-        if mid == n:
-            return f'Position : {mid-1}'
-        elif mid > n:
-            binary_search(lst,n,start = start,end = end - 1)
-        elif mid < n:
-            binary_search(lst,n,start = start + 1, end = end)
+class ArrayModification:
             
-    
     def linear_search(self,lst,n):
         for i in range(len(lst)):
             if lst[i] == n:
                 return f'Position :{i}'
         return -1
     
-    def run_search(self):
-        while True:
-            print('Select the searching algorithm:')
-            print('1. Linear Search.')
-            print('2. Binary Search.')
-            print('3. quit.')
-            opt = int(input('Option: '))
-            if opt == 2:
-                print(search.binary_search(self.lst,self.number))
-            elif opt == 1:
-                print(search.linear_search(self.lst,self.number))
-            elif opt == 3:
-            	break
+    def insertion_sort(self,lst):
+        for i in range(len(lst)):
+            
+            index = lst[i]
+            
+            k = i - 1
+            
+            while k >= 0 and lst[k] > index:
+                lst[k + 1] = lst[k]
+                k -= 1
+                
+            lst[k+1]  = index
+            
+        return lst
+    
+    def merge(self,lst1,lst2):
+        return ArrayModification.insertion_sort(lst1 + lst2)
+    
+    def reverse(self,lst):
+        return lst[::-1]
+
+lst = [2,9,1,7,3,5,2]
+Arrmod = ArrayModification()
+print(Arrmod.linear_search(lst,3))
         
 
-lst = [1,2,3,4,5,6,7,8]
-number = 4
-search = Search(lst,number)
-search.run_search()
